20edbee5f8ce9ad8510bbabae06391a6c855aaf1
Tatsu015 <tatsumi.soichiro@gmail.com>
2022-09-03 06:28:59 +0000

    japanese read OK!

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from PIL import Image
+import pyocr
 import pytesseract
+import cv2
 
-FILENAME = './data/img.jpeg'
-print(pytesseract.image_to_string(Image.open(FILENAME),config='digits'))
+# image = cv2.imread('./data/iraUG.jpeg')
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# data = pytesseract.image_to_string(gray, lang='eng', config='--psm 6').strip()
+# print(data)
+# print('--------------------------')
+
+# num = pytesseract.image_to_string(Image.open('./data/img.jpeg'),config='digits').strip()
+# print(num)
+# print('--------------------------')
+
+# longj = pytesseract.image_to_string(Image.open('./data/pyocr_test.png'),lang='jpn').strip()
+# print(longj)
+# print('--------------------------')
+
+
+tools = pyocr.get_available_tools()
+tool = tools[0]
+jpn = tool.image_to_string(Image.open('./data/jpn_resize.png'),lang='jpn',builder=pyocr.builders.TextBuilder(tesseract_layout=3)).strip()
+
+print(jpn)
+
